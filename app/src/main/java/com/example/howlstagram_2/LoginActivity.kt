8102e5f1e7ae1ea9da1001c7bc91228420b52a63
chae <jungchae5812@sookmyung.ac.kr>
2022-08-24 20:45:01 +0900

    add toolbar

--- app/src/main/java/com/example/howlstagram_2/LoginActivity.kt
+++ app/src/main/java/com/example/howlstagram_2/LoginActivity.kt
@@ -1,11 +1,96 @@
 package com.example.howlstagram_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    var auth : FirebaseAuth? = null
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001 //로그인 시 사용할 리퀘스트 코드
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+        email_login_button.setOnClickListener {
+            signinAndSignup()
+        }
+        google_sign_in_buttonn.setOnClickListener {
+            //first step
+            googleLogin()
+        }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)) //구글 api 길 넣어주기?
+            .requestEmail() //email 아이디 받아오기
+            .build() //닫아주기
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+    }
+    fun googleLogin(){
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent,GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE){
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data!!)
+            if(result!!.isSuccess){
+                var account = result.signInAccount
+                //second step
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+    fun firebaseAuthWithGoogle(account : GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){  //로그인 성공
+                    moveMainPage(task.result?.user)
+                }else{  //로그인 실패
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    fun signinAndSignup(){ //회원가입
+        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
+            ?.addOnCompleteListener {
+                task ->
+                    if(task.isSuccessful){  //계정 생성
+                        moveMainPage(task.result.user)
+                    }else if(task.exception?.message.isNullOrEmpty()){  //입력 x 일 때
+                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                    }else{  //계정이 있는 경우
+                        signinEmail()
+                    }
+            }
+    }
+    fun signinEmail(){  //로그인
+        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){  //로그인 성공
+                    moveMainPage(task.result?.user)
+                }else{  //로그인 실패
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+    fun moveMainPage(user:FirebaseUser?){   //로그인 성공 시 페이지 전환
+        if(user != null){
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
